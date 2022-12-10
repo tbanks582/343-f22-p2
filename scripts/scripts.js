9183c7a01ce144d4b0a3c0b7646b4e470675d64a
trevor nicholas <trevornicholas@trevors-MacBook-Pro.local>
2022-12-09 19:52:51 -0500

    finished the report and styled my page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -44,7 +44,7 @@ function getDefault(){
     var url = "https://api.infermedica.com/v3/diagnosis";
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.setRequestHeader("App-Id" , "745a521a");
+    xhr.setRequestHeader("App-Id" , "745a521a"); // remove these immediately after turning in :)
     xhr.setRequestHeader("App-Key" , "706c4c10d0dcb1d4f40d3cd705da4cbd");
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
@@ -66,16 +66,20 @@ function getSearch(evidence) {
             document.getElementById('query2').value,
             document.getElementById('query3').value,
         ]
+        console.log(evidence);
+        
         let symptoms = "i feel " + evidence[0] + ", as well as " + evidence[1] + ", and probably " +evidence[2];
         console.log(symptoms);
         let text = {"text": symptoms, "age": {"value": 30}};
         var xhr = new XMLHttpRequest();
         xhr.onload = function(){
             var jsonResponse = JSON.parse(xhr.responseText);
-            let sympResponse = jsonResponse.mentions.map( symptom => parseInt(symptom.id.substring(2),10)%255);
+            
+            let sympResponse = jsonResponse.mentions.length >= 3 ? jsonResponse.mentions.map( symptom => parseInt(symptom.id.substring(2),10)%255): evidence = [1193, 488, 418];
             console.log(jsonResponse);
             console.log("symp numbers = " + sympResponse);
             colorSearch(sympResponse);
+            getInfermedica(sympResponse);
             
         }
 
@@ -89,20 +93,66 @@ function getSearch(evidence) {
     }
 }
     
-function getInfermedica() {
-    let request = new Request('https://api.infermedica.com/v3/diagnosis',
-         { method: 'POST', 
-           headers: new Headers({ 'Content-Type': 'application/json', 'App-Id':'745a521a', 'App-Key':'706c4c10d0dcb1d4f40d3cd705da4cbd'}) 
-         });
-    // Now use it! 
+function getInfermedica(symptoms) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function(){
+        var jsonResponse = JSON.parse(xhr.responseText);
+        console.log('aaaaaa');
+        //console.log(jsonResponse);
+        let btw = document.getElementById('btw');
+        btw.innerHTML = '';            
+        
+        if(jsonResponse.conditions.length>0){
+            console.log('condition found');
 
-   fetch(request) 
-   .then(resp => resp.json())
-   .then (data => console.log(data))
-   .catch(err => { 
-         // handle errors 
-         console.log("get request failed!");
-    });
+            for(i = 0;i<jsonResponse.conditions.length;i++){
+                btw.innerHTML += 
+                `
+                    <li>${jsonResponse.conditions[i].common_name}</li>
+                `
+            }
+            document.getElementById("warning").innerHTML="<strong>btw these are some possible conditions you may be experiencing. go talk to your doctor :)</strong>"
+        }
+        console.log(jsonResponse);
+    }
+    // Now use it! 
+    var data = {
+        "sex": "male",
+        "age": {
+        "value": 30
+        },
+        "evidence": [
+            {
+                "id": ("s_" + symptoms[0]),
+                "choice_id":"present",
+                "source":"initial",
+            },
+            {
+                "id": ("s_" + symptoms[1]),
+                "choice_id":"present",
+            },
+            {
+                "id": ("s_" + symptoms[2]),
+                "choice_id":"present",
+            }
+        ]
+    }
+
+
+
+    var url = "https://api.infermedica.com/v3/diagnosis";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("App-Id" , "745a521a");
+    xhr.setRequestHeader("App-Key" , "706c4c10d0dcb1d4f40d3cd705da4cbd");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var json = JSON.parse(xhr.responseText);
+            
+        }
+    }
+    var dataJSON = JSON.stringify(data);
+    xhr.send(dataJSON);
 }
 
 function colorSearch(arr){
@@ -117,6 +167,7 @@ function colorSearch(arr){
 
     xhr.onload = function() {
         console.log(data);
+        console.log(xhr.responseText);
         let jason = JSON.parse(xhr.responseText);
         console.log(jason);
         colorPrinter(jason);
@@ -136,7 +187,7 @@ function colorPrinter(palette){
         let element = palette.result[i];
         document.getElementById("color"+ (i+1)).style.background = ("rgb(" + element[0] + "," + element[1] + "," + element[2] + ")");
     }
-    document.getElementById("mood").innerHTML="<p>here are your mood colors!</p>";
+    document.getElementById("mood").innerHTML="<p><strong>here are your mood colors!<strong></p>";
     return colors;
 }
 
